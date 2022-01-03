2c0bc3ca5707afd0508058b73869e386f7a30404
Joao4569 <johnnyrod4569@gmail.com>
2022-01-03 15:27:41 +0000

    feat: Change some class names to id's in order to create specificity and add username text input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
-function setUsername() {
+document.getElementById("start-button").addEventListener("click", function() {
+    setUsername();
+})
 
+/**
+ * Once the user initiates play, then the user can select a username
+ */
+function setUsername() {
+    document.getElementById("game-area").innerHTML = `
+        <div>
+            <p>Create a Username:</p>
+        </div>
+        <form action="">
+        <label for="Username"></label>
+        <input type="text" id="Username" name="Username">
+        </form>
+        `;
 }
 
 function displayQuestion() {
